0c2bf48652d5d77bd9585d2b7253ff13f06fbfa9
Rodans3k <michal.drzewi@gmail.com>
2023-08-27 19:23:26 +0200

    almost finished GET endpoint tests

--- app/test/app.test.ts
+++ app/test/app.test.ts
@@ -349,20 +349,398 @@ describe("Testing /movies GET endpoint", () => {
       expect(response.body).toEqual(expectedResponseBody);
     });
     it("Returns 200 with matching list of movies", async () => {
-      expect(1).toBe(2);
+      // Prepare
+      const expectedResponseBody = [
+        { ...exampleMovies[0], runtime: "55" },
+        { ...exampleMovies[1], runtime: "65" },
+      ];
+      const initialDbContent: MovieJsonObject = {
+        genres: allowedGenres,
+        movies: [
+          { ...exampleMovies[3], runtime: "49" },
+          { ...exampleMovies[4], runtime: "71" },
+        ],
+      };
+      const searchParameters = {
+        runtime: `60`,
+        genres: undefined,
+      };
+      appMockedDb.setDbData(initialDbContent);
+      const app = createApp(appMockedDb);
+
+      // Execute
+      const response = await request(app)
+        .get(`${endpoint}`)
+        .query(searchParameters);
+
+      // Compare
+      expect(response.statusCode).toEqual(200);
+      expect(response.body.length).toEqual(expectedResponseBody.length);
+      response.body.forEach((responseMovie) => {
+        expect(expectedResponseBody).toHaveBeenCalledWith(
+          expect.objectContaining(responseMovie),
+        );
+      });
     });
     it("Returns 200 with matching list of movies, contains edge cases", async () => {
-      expect(1).toBe(2);
+      // Prepare
+      const expectedResponseBody = [
+        { ...exampleMovies[0], runtime: "50" },
+        { ...exampleMovies[1], runtime: "70" },
+      ];
+      const initialDbContent: MovieJsonObject = {
+        genres: allowedGenres,
+        movies: [
+          { ...exampleMovies[3], runtime: "49" },
+          { ...exampleMovies[4], runtime: "71" },
+        ],
+      };
+      const searchParameters = {
+        runtime: `60`,
+        genres: undefined,
+      };
+      appMockedDb.setDbData(initialDbContent);
+      const app = createApp(appMockedDb);
+
+      // Execute
+      const response = await request(app)
+        .get(`${endpoint}`)
+        .query(searchParameters);
+
+      // Compare
+      expect(response.statusCode).toEqual(200);
+      expect(response.body.length).toEqual(expectedResponseBody.length);
+      response.body.forEach((responseMovie) => {
+        expect(expectedResponseBody).toHaveBeenCalledWith(
+          expect.objectContaining(responseMovie),
+        );
+      });
     });
   });
   describe("Testing genre and runtime search", () => {
-    it("Returns 404 if no movies in DB ", async () => {
+    it("Returns 200 with empty list if no movies are present in DB ", async () => {
       // Prepare
+      const expectedResponseBody = [];
+      const initialDbContent: MovieJsonObject = {
+        genres: allowedGenres,
+        movies: [],
+      };
+      const searchParameters = {
+        runtime: undefined,
+        genres: allowedGenres[0],
+      };
+      appMockedDb.setDbData(initialDbContent);
+      const app = createApp(appMockedDb);
 
-      // Executa
+      // Execute
+      const response = await request(app)
+        .get(`${endpoint}`)
+        .query(searchParameters);
 
       // Compare
-      expect(1).toBe(2);
+      expect(response.statusCode).toEqual(200);
+      expect(response.body).toEqual(expectedResponseBody);
+    });
+    it.each([
+      [
+        {
+          genres: allowedGenres,
+          movies: [
+            {
+              ...exampleMovies[0],
+              genres: [allowedGenres[1], allowedGenres[2]],
+              runtime: "60",
+            },
+            {
+              ...exampleMovies[2],
+              genres: [allowedGenres[1], allowedGenres[2]],
+              runtime: "60",
+            },
+          ],
+        },
+        {
+          runtime: "60",
+          genres: allowedGenres[0],
+        },
+        [],
+        {
+          genres: allowedGenres,
+          movies: [
+            {
+              ...exampleMovies[0],
+              genres: [allowedGenres[0], allowedGenres[2]],
+              runtime: "90",
+            },
+            {
+              ...exampleMovies[2],
+              genres: [allowedGenres[0]],
+              runtime: "20",
+            },
+          ],
+        },
+        {
+          runtime: "60",
+          genres: allowedGenres[0],
+        },
+        [],
+      ],
+    ])(
+      "Returns 200 with empty list if no movies match the search criteria",
+      async (
+        initialDbContent: MovieJsonObject,
+        searchParameters: { runtime: string; genres: string },
+        expectedResponseBody: Array<any>,
+      ) => {
+        // Prepare
+        appMockedDb.setDbData(initialDbContent);
+        const app = createApp(appMockedDb);
+
+        // Execute
+        const response = await request(app)
+          .get(`${endpoint}`)
+          .query(searchParameters);
+
+        // Compare
+        expect(response.statusCode).toEqual(200);
+        expect(response.body).toEqual(expectedResponseBody);
+      },
+    );
+    it.each([
+      [
+        {
+          genres: allowedGenres,
+          movies: [
+            {
+              ...exampleMovies[0],
+              genres: [allowedGenres[0], allowedGenres[1]],
+              runtime: "60",
+            },
+            {
+              ...exampleMovies[2],
+              genres: [allowedGenres[2], allowedGenres[3]],
+              runtime: "60",
+            },
+          ],
+        },
+        {
+          runtime: "60",
+          genres: `${allowedGenres[0]}, ${allowedGenres[5]}`,
+        },
+        [{ ...exampleMovies[0], genres: [allowedGenres[0], allowedGenres[1]] }],
+      ],
+      [
+        {
+          genres: allowedGenres,
+          movies: [
+            {
+              ...exampleMovies[0],
+              genres: [allowedGenres[0], allowedGenres[1]],
+              runtime: "60",
+            },
+            {
+              ...exampleMovies[2],
+              genres: [allowedGenres[0], allowedGenres[5]],
+              runtime: "80",
+            },
+          ],
+        },
+        {
+          runtime: "60",
+          genres: `${allowedGenres[0]}, ${allowedGenres[5]}`,
+        },
+        [{ ...exampleMovies[0], genres: [allowedGenres[0], allowedGenres[1]] }],
+      ],
+    ])(
+      "Returns 200 and list with single matching movie",
+      async (
+        initialDbContent: MovieJsonObject,
+        searchParameters: { runtime: string; genres: string },
+        expectedResponseBody: Array<any>,
+      ) => {
+        // Prepare
+        appMockedDb.setDbData(initialDbContent);
+        const app = createApp(appMockedDb);
+
+        // Execute
+        const response = await request(app)
+          .get(`${endpoint}`)
+          .query(searchParameters);
+
+        // Compare
+        expect(response.statusCode).toEqual(200);
+        expect(response.body).toEqual(expectedResponseBody);
+      },
+    );
+    it.each([
+      [
+        {
+          genres: allowedGenres,
+          movies: [
+            {
+              ...exampleMovies[0],
+              id: 11,
+              genres: [allowedGenres[0], allowedGenres[1], allowedGenres[2]],
+              runtime: "60",
+            },
+            {
+              ...exampleMovies[1],
+              id: 12,
+              genres: [allowedGenres[0]],
+              runtime: "60",
+            },
+            {
+              ...exampleMovies[2],
+              id: 13,
+              genres: [allowedGenres[3]],
+              runtime: "60",
+            },
+            {
+              ...exampleMovies[3],
+              id: 14,
+              genres: [allowedGenres[0], allowedGenres[1]],
+              runtime: "60",
+            },
+          ],
+        },
+        {
+          runtime: "60",
+          genres: `${allowedGenres[0]}, ${allowedGenres[1]}, ${allowedGenres[2]}`,
+        },
+        [
+          {
+            ...exampleMovies[0],
+            id: 11,
+            genres: [allowedGenres[0], allowedGenres[1], allowedGenres[2]],
+            runtime: "60",
+          },
+          {
+            ...exampleMovies[3],
+            id: 14,
+            genres: [allowedGenres[0], allowedGenres[1]],
+            runtime: "60",
+          },
+          {
+            ...exampleMovies[1],
+            id: 13,
+            genres: [allowedGenres[0]],
+            runtime: "60",
+          },
+        ],
+      ],
+      [
+        {
+          genres: allowedGenres,
+          movies: [
+            {
+              ...exampleMovies[0],
+              id: 11,
+              genres: [allowedGenres[0], allowedGenres[1], allowedGenres[2]],
+              runtime: "80",
+            },
+            {
+              ...exampleMovies[1],
+              id: 12,
+              genres: [allowedGenres[0]],
+              runtime: "60",
+            },
+            {
+              ...exampleMovies[2],
+              id: 13,
+              genres: [allowedGenres[3]],
+              runtime: "60",
+            },
+            {
+              ...exampleMovies[3],
+              id: 14,
+              genres: [allowedGenres[0], allowedGenres[1]],
+              runtime: "60",
+            },
+          ],
+        },
+        {
+          runtime: "60",
+          genres: `${allowedGenres[0]}, ${allowedGenres[1]}, ${allowedGenres[2]}`,
+        },
+        [
+          {
+            ...exampleMovies[3],
+            id: 14,
+            genres: [allowedGenres[0], allowedGenres[1]],
+            runtime: "60",
+          },
+          {
+            ...exampleMovies[1],
+            id: 13,
+            genres: [allowedGenres[0]],
+            runtime: "60",
+          },
+        ],
+      ],
+    ])(
+      "Returns 200 and list with multiple matching movies, ordered from most matching to least",
+      async (
+        initialDbContent: MovieJsonObject,
+        searchParameters: { runtime: string; genres: string },
+        expectedResponseBody: Array<any>,
+      ) => {
+        // Prepare
+        appMockedDb.setDbData(initialDbContent);
+        const app = createApp(appMockedDb);
+
+        // Execute
+        const response = await request(app)
+          .get(`${endpoint}`)
+          .query(searchParameters);
+
+        // Compare
+        expect(response.statusCode).toEqual(200);
+        expect(response.body.length).toEqual(expectedResponseBody.length);
+        for (let index = 0; index < response.body.length; index++) {
+          expect(response.body[index]).toEqual(expectedResponseBody[index]);
+        }
+      },
+    );
+    it("Returns 400 for incorrect genres in query", async () => {
+      // Prepare
+      const initialDbContent: MovieJsonObject = {
+        genres: allowedGenres,
+        movies: exampleMovies,
+      };
+      const searchParameters = {
+        runtime: "80",
+        genres: `${illegalGenres[0]}`,
+      };
+      appMockedDb.setDbData(initialDbContent);
+      const app = createApp(appMockedDb);
+
+      // Execute
+      const response = await request(app)
+        .get(`${endpoint}`)
+        .query(searchParameters);
+
+      // Compare
+      expect(response.statusCode).toEqual(400);
+    });
+    it("Returns 400 for mix of correct & incorrect genres", async () => {
+      // Prepare
+      const initialDbContent: MovieJsonObject = {
+        genres: allowedGenres,
+        movies: exampleMovies,
+      };
+      const searchParameters = {
+        runtime: "80",
+        genres: `${illegalGenres[0]}, ${allowedGenres[0]}`,
+      };
+      appMockedDb.setDbData(initialDbContent);
+      const app = createApp(appMockedDb);
+
+      // Execute
+      const response = await request(app)
+        .get(`${endpoint}`)
+        .query(searchParameters);
+
+      // Compare
+      expect(response.statusCode).toEqual(400);
     });
   });
 });
